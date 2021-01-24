181006505ada59ea075ec515b1473a34bce92529
mariajose <mariajose.simon@ikasle.egibide.org>
2021-01-24 19:26:23 +0100

    Proyecto con gitignore swift y xcode

---
+++ aparcamientosCocoaPods/aparcamientosCocoaPods/aparcamientosCocoaPodsApp.swift
@@ -0,0 +1,17 @@
+//
+//  aparcamientosCocoaPodsApp.swift
+//  aparcamientosCocoaPods
+//
+//  Created by mariajose on 24/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct aparcamientosCocoaPodsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
